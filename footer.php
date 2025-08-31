a35bae052423ac234858fc40d16a4adad3e717de
satriajakaprayoga0707 <satriajakaprayoga0707@gmail.com>
2025-08-31 21:50:04 +0700

    feat: show social medias in all page

--- footer.php
+++ footer.php
@@ -4,6 +4,45 @@
   </div>
 </footer>
 <?php wp_footer(); ?>
+<div class="ebookgua-social-fixed">
+    <?php
+
+      $socials = [
+          'facebook' => [
+              'icon' => 'fab fa-facebook-f',
+              'color' => '#1877F2',
+          ],
+          'twitter' => [
+              'icon' => 'fab fa-twitter',
+              'color' => '#1DA1F2',
+          ],
+          'instagram' => [
+              'icon' => 'fab fa-instagram',
+              'color' => '#E4405F',
+          ],
+          'linkedin' => [
+              'icon' => 'fab fa-linkedin-in',
+              'color' => '#0A66C2',
+          ],
+          'youtube' => [
+              'icon' => 'fab fa-youtube',
+              'color' => '#FF0000',
+          ],
+      ];
+
+    foreach ($socials as $key => $data) {
+        $url = get_theme_mod("ebookgua_social_{$key}");
+        $color = get_theme_mod("ebookgua_social_{$key}_color", $data['color']);
+
+        if ($url) {
+            echo '<a href="'.esc_url($url).'" target="_blank" rel="noopener" 
+                style="color:'.esc_attr($color).'; margin-right:10px; font-size:20px;">';
+            echo '<i class="'.esc_attr($data['icon']).'"></i>';
+            echo '</a>';
+        }
+    }
+    ?>
+</div>
 </body>
 </html>
 <script>
